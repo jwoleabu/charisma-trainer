4a00e03d23638d27cbad89d2ca70dedeeb4c9e0f
Pythe <jwoleabu@gmail.com>
2024-02-07 21:51:21 +0000

    react blitz initialization

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import BlitzButton from "./components/BlitzButton.tsx";
+
+function App() {
+
+  return (
+      <div>
+        <BlitzButton/>
+      </div>
+  )
+}
+
+export default App
